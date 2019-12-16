e9e90ba77632c625752d7036126045537ddc32ab
Skander bahri <Skander.bahri@hotmail.com>
2019-12-16 15:34:34 +0100

    initial commit

--- Machine Learning/ViewController.swift
+++ Machine Learning/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else
+            {
+                fatalError("Could't convet the selected image into CIImage")
+            }
+            detect(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage)
+    {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else
+        {
+             fatalError("Loading Core ML model Failed. ")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else
+            {
+                fatalError("Casting result as VNClassificationObservation has been broken")
+            }
+            if let firstResult =  results.first
+            {
+                //self.navigationController?.navigationBar.barTintColor = UIColor.green
+                var confienceValue = firstResult.confidence
+                confienceValue = confienceValue * 100
+                let percentage = Int(confienceValue)
+                self.navigationItem.title = String(percentage) + "% " + firstResult.identifier
+                if ( percentage > 60 )  {
+                    self.navigationController?.navigationBar.barTintColor = UIColor.green
+                }else if ( percentage < 60 ) && ( percentage > 45 ) {
+                    self.navigationController?.navigationBar.barTintColor = UIColor.yellow
+                }else{
+                    self.navigationController?.navigationBar.barTintColor = UIColor.red
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch  {
+            print(error)
+        }
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem)
+    {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
